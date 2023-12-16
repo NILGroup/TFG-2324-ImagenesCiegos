4e9e8144f07c5569f1ee915c9adef376beaf2612
Walid Bousnitra Bousnitra <walidbou@ucm.es>
2023-12-16 19:27:17 +0100

    recuperado

---
+++ app/app/src/main/java/com/android/app/MainActivity.java
@@ -0,0 +1,211 @@
+package com.android.app;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.firebase.database.DatabaseReference;
+
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFunctions fFunc = FirebaseFunctions.getInstance();
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageReference = storage.getReference();
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().child("imagenes");
+    private Uri imageUri;
+    ImageView ivPicture;
+    TextView tvResult;
+    TextView awita;
+    Button btnChoosePicture;
+
+    private static final int CAMERA_PERMISSION_CODE=223;
+    private static final int READ_STORAGE_PERMISSION_CODE=144;
+    private static final int WRITE_STORAGE_PERMISSION_CODE=144;
+    private static final String TAG = "Numero Objetos detectados";
+
+    ActivityResultLauncher<Intent> cameraLauncher;
+    ActivityResultLauncher<Intent>galleryLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivPicture = findViewById(R.id.ivPicture);
+        tvResult = findViewById(R.id.tvResult);
+        awita = findViewById(R.id.awita);
+        btnChoosePicture = findViewById(R.id.btnChoosePicture);
+        cameraLauncher=registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Intent data = result.getData();
+                        try {
+                            Bitmap photo=(Bitmap) data.getExtras().get("data");
+                            ivPicture.setImageBitmap(photo);
+                            imageUri = data.getData();
+                            uploadPicture(imageUri);
+                        }catch (Exception e){
+                            Log.d(TAG,"onActivityResult:"+ e.getMessage());
+                        }
+                    }
+                }
+
+        );
+        galleryLauncher=registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Intent data = result.getData();
+                        try {
+                            ivPicture.setImageURI(data.getData());
+                            imageUri = data.getData();
+                            uploadPicture(imageUri);
+                        }catch (Exception e){
+                            Log.d(TAG,"onActivityResult:"+ e.getMessage());
+                        }
+                    }
+                }
+
+        );
+        btnChoosePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String [] options={"camera","gallery"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Elige una Opcion");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if( which==0){
+                            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            cameraLauncher.launch(cameraIntent);
+                        }else{
+                            Intent storageIntent = new Intent();
+                            storageIntent.setType("image/*");
+                            storageIntent.setAction(Intent.ACTION_GET_CONTENT);
+                            galleryLauncher.launch(storageIntent);
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        CheckPermission(android.Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE);
+    }
+    public void CheckPermission(String permission, int requestCode){
+        if(ContextCompat.checkSelfPermission(MainActivity.this,permission)== PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission},requestCode);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode,@NonNull String[] permissions,@NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(MainActivity.this, "Permiso de cámara denegado", Toast.LENGTH_SHORT).show();
+            } else {
+                CheckPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE, READ_STORAGE_PERMISSION_CODE);
+            }
+        } else if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(MainActivity.this, "Permiso de cámara denegado", Toast.LENGTH_SHORT).show();
+            } else {
+                CheckPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, WRITE_STORAGE_PERMISSION_CODE);
+            }
+        } else if (requestCode == WRITE_STORAGE_PERMISSION_CODE) {
+            if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(MainActivity.this, "Permiso de cámara denegado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    //funcion que llama a la cloud function
+    private Task<String> callImagen(String name){
+        Map<String,Object> data = new HashMap<>();
+        data.put("url",name);
+
+        return fFunc.getHttpsCallable("descripImagen")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+                    @Override
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        return (String) task.getResult().getData();
+                    }
+                });
+    }
+
+    private void uploadPicture(Uri uri){
+        StorageReference fileRef = storageReference.child(System.currentTimeMillis()+ "."+ getFileextension(uri));
+        fileRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Model model = new Model(uri.toString());
+                        String modelId = root.push().getKey();
+                        root.child(modelId).setValue(model);
+                        callImagen(uri.toString()).addOnCompleteListener(new OnCompleteListener<String>() {
+                            @Override
+                            public void onComplete(@NonNull Task<String> task) {
+                                awita.setText(task.getResult());
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    private String getFileextension(Uri muri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(muri));
+    }
+}
